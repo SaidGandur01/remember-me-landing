158fed7fb1c91aafd5edd032f8e312390969e1aa
Said Gandur <ferneyvaron0@gmail.com>
2024-02-03 13:08:40 -0500

    add slider for relatos, and mobile view

--- components/sectionFour.vue
+++ components/sectionFour.vue
@@ -1,31 +1,96 @@
 <template>
   <section class="section-four">
-    <h1>Encontrar historias de acuerdo a tus intereses</h1>
-    <p>
-      Los niños podrán navegar por un gran listado de historias que permitirá abrir la imaginación, conocer historias fascinantes
-      y llevar grandes recuerdos
-    </p>
-    <div class="image-container">
-      <img src="~/public/images/section-four/kids.png" alt="kids">
-    </div>
-    <h1>Relatos</h1>
-    <div class="relatos-container">
+    <div class="section-four-container">
+      <h2>Encontrar historias de acuerdo a tus intereses</h2>
+      <p>
+        Los niños podrán navegar por un gran listado de historias que permitirá
+        abrir la imaginación, conocer historias fascinantes y llevar grandes
+        recuerdos
+      </p>
       <div class="image-container">
-        <img src="~/public/images/section-four/father.png" alt="man">
+        <img src="~/public/images/section-four/kids.png" alt="kids" />
       </div>
-      <div class="comment">
-        <h2>martin javier leon</h2>
-        <p>
-          Me encanta la oportunidad de contar mi vida, <br> que pequeños niños puedan entender por lo que pasamos y asi <br> poder quedar en sus memorias
-        </p>
-        <span>
-          Leer la historia completa
-        </span>
+      <h2>Relatos</h2>
+      <div class="relatos-container">
+        <Swiper
+          :pagination="{
+            dynamicBullets: true,
+            clickable: true,
+          }"
+          :autoplay="{
+            delay: 3000,
+            disableOnInteraction: false,
+          }"
+          :modules="modules"
+        >
+          <SwiperSlide>
+            <div class="image-container">
+              <img src="~/public/images/section-four/father.png" alt="man" />
+            </div>
+            <div class="comment">
+              <h2>martin javier leon</h2>
+              <p>
+                Me encanta la oportunidad de contar mi vida, <br />
+                que pequeños niños puedan entender por lo que pasamos y asi <br />
+                poder quedar en sus memorias
+              </p>
+              <span> Leer la historia completa </span>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="image-container">
+              <img src="~/public/images/section-four/father.png" alt="man" />
+            </div>
+            <div class="comment">
+              <h2>martin javier leon</h2>
+              <p>
+                Me encanta la oportunidad de contar mi vida, <br />
+                que pequeños niños puedan entender por lo que pasamos y asi <br />
+                poder quedar en sus memorias
+              </p>
+              <span> Leer la historia completa </span>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="image-container">
+              <img src="~/public/images/section-four/father.png" alt="man" />
+            </div>
+            <div class="comment">
+              <h2>martin javier leon</h2>
+              <p>
+                Me encanta la oportunidad de contar mi vida, <br />
+                que pequeños niños puedan entender por lo que pasamos y asi <br />
+                poder quedar en sus memorias
+              </p>
+              <span> Leer la historia completa </span>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="image-container">
+              <img src="~/public/images/section-four/father.png" alt="man" />
+            </div>
+            <div class="comment">
+              <h2>martin javier leon</h2>
+              <p>
+                Me encanta la oportunidad de contar mi vida, <br />
+                que pequeños niños puedan entender por lo que pasamos y asi <br />
+                poder quedar en sus memorias
+              </p>
+              <span> Leer la historia completa </span>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   </section>
 </template>
 <script lang="ts" setup>
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Pagination, Autoplay } from 'swiper/modules'
+
+const modules = [Pagination, Autoplay]
 </script>
 <style lang="scss" scoped>
 .section-four {
@@ -33,8 +98,12 @@
   min-height: 145vh;
   position: relative;
   overflow: hidden;
+  .section-four-container {
+    display: flex;
+    flex-direction: column;
+  }
 
-  h1 {
+  h2 {
     margin: 3rem 0 5rem;
     color: var(--color-brand-implemented-200);
     text-align: center;
@@ -63,18 +132,26 @@
   }
 
   .relatos-container {
-    position: relative;
     padding: 0 10rem 0 20rem;
+    .swiper {
+      width: 100%;
+      height: auto;
+    }
+    .swiper-slide {
+      display: flex;
+      flex-direction: row;
+    }
+    .swiper > :first-child {
+      margin-bottom: 6rem;
+    }
     .image-container {
-      width: 50%;
+      width: 650px;
       margin: 0;
-
       img {
         width: 100%;
         height: 100%;
       }
     }
-
     .comment {
       width: fit-content;
       padding: 4rem;
@@ -94,6 +171,7 @@
 
       h2 {
         color: var(--color-text-gray);
+        margin: 1rem 0;
         font-size: 2rem;
         font-weight: 700;
         line-height: 100%;
@@ -121,9 +199,42 @@
       }
     }
   }
+  @media (max-width: 768px) {
+    padding: 7rem 0;
+    .section-four-container {
+      gap: 3rem;
+      min-height: auto;
+      margin: 0 4rem;
+    }
+    h2 {
+      font-size: 3.6rem;
+      margin: 0;
+    }
+    .image-container {
+      width: 100%;
+      margin: 0;
+    }
+    .relatos-container {
+      padding: 0;
+      .swiper > :first-child {
+        margin-bottom: 3rem;
+      }
+      .swiper-slide {
+        flex-direction: column;
+      }
+      .image-container {
+        width: 100%;
+      }
+      .comment {
+        position: unset;
+        transform: translateY(-10%);
+        margin: 0 2rem;
+      }
+    }
+  }
 }
 .section-four::before {
-  content: '';
+  content: "";
   z-index: -1001;
   width: 100%;
   height: 100%;
