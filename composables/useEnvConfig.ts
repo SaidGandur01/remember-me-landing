744644ee79b9c0e8f45478a148b5cb09a6e98701
Said Gandur <saidgandur@MacBook-Pro-de-Said.local>
2024-08-07 15:43:59 -0500

    Updating useEnvCOnfig composable

--- composables/useEnvConfig.ts
+++ composables/useEnvConfig.ts
@@ -3,10 +3,16 @@ export const useEnvConfig = () => {
 
   const isProd = process.env.NODE_ENV === 'production'
 
-  const endpoint = isProd ? config.public.prodEndpoint : config.public.sandboxEndpoint
-  const publicKey = isProd ? config.public.prodPublicKey : config.public.sandboxPublicKey
-  const secretIntegrityKey = isProd ? config.prodSecretIntegrityKey : config.sandboxSecretIntegrityKey
-  const secretEventsKey = isProd ? config.prodSecretEventsKey : config.sandboxSecretEventsKey
+  // ORIGINAL
+  // const endpoint = isProd ? config.public.prodEndpoint : config.public.sandboxEndpoint
+  // const publicKey = isProd ? config.public.prodPublicKey : config.public.sandboxPublicKey
+  // const secretIntegrityKey = isProd ? config.prodSecretIntegrityKey : config.sandboxSecretIntegrityKey
+  // const secretEventsKey = isProd ? config.prodSecretEventsKey : config.sandboxSecretEventsKey
+
+  const endpoint = isProd ? config.public.sandboxEndpoint : config.public.prodEndpoint
+  const publicKey = isProd ? config.public.sandboxPublicKey : config.public.prodPublicKey
+  const secretIntegrityKey = isProd ? config.sandboxSecretIntegrityKey : config.prodSecretIntegrityKey
+  const secretEventsKey = isProd ? config.sandboxSecretEventsKey : config.prodSecretEventsKey
 
   return {
     endpoint,
