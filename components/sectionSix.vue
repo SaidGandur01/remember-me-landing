442d05d98c33276c01368f5baad3894ccb444e16
Said Gandur <saidgandur@MacBook-Pro-de-Said.local>
2024-08-07 15:32:09 -0500

    test

--- components/sectionSix.vue
+++ components/sectionSix.vue
@@ -61,7 +61,7 @@ const amount = ref<number>(0)
 const customAmount = ref<string>('')
 const currency = 'COP'
 // const expirationTime = '2023-06-09T20:28:50.000Z'
-
+console.log('test')
 const hash = ref('')
 
 const selectAmount = (value: number) => {
