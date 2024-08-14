0f77d0a0d35c3bc70e7f7a73620d23cd66495cb6
Said Gandur <saidgandur@MacBook-Pro-de-Said.local>
2024-08-13 21:49:41 -0500

    Updating nuxt config file to initial scale of 1.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,10 +10,7 @@ export default defineNuxtConfig({
         lang: 'es'
       },
       meta: [
-        {
-          name: 'viewport',
-          content: 'width=device-width, initial-scale=1'
-        },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"' },
         {
           charset: 'utf-8'
         }
